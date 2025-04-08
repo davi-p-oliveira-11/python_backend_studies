b61774cf3b8888c4b8871e6269796605b5c734f1
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-07 23:37:24 -0300

    updated connection script

--- course_01/banco_de_dados/connection.py
+++ course_01/banco_de_dados/connection.py
@@ -3,5 +3,28 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
 
-connection = sqlite3.connect(ROOT_PATH / "clients.db")
-print(connection)
+connection = sqlite3.connect(ROOT_PATH / "my_database.db")
+cursor = connection.cursor()
+
+def create_table(connection, cursor):
+    cursor.execute("CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))")
+    connection.commit()
+
+
+def insert_registry(connection, cursor, nome, email):
+    data = (nome, email)
+    cursor.execute(f"INSERT INTO clientes (nome, email) VALUES (?,?);", data)
+    connection.commit()
+
+def update_registry(connection, cursor, nome, email, id):
+    data = (nome, email, id)
+    cursor.execute("UPDATE clientes SET nome=?, email=? WHERE id=?;", data)
+    connection.commit()
+
+def delete_registry(connection, cursor, id):
+    data = (id,)
+    cursor.execute("DELETE FROM clientes WHERE id=?;", data)
+    connection.commit()
+
+# update_registry(connection, cursor, "John Bonham", "johnled@gmail.com", 1)
+delete_registry(connection, cursor, 1)
