3099e0f81d068b0386a754b497626c63da3c306e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-12 22:21:06 -0300

    updated app.py

--- course_01/APIs_with_flask/greenbank/src/app.py
+++ course_01/APIs_with_flask/greenbank/src/app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-  return "<h1>Hello, World! </h1>"
+@app.route("/helloworld/<user>/<int:age>/<float:height>")
+def hello_world(user, age, height):
+  print(age)
+  print(f"Type of the variable age: {type(age)}")
+  print(f"Type of the variable user: {type(user)}")
+  print(f"Type of the variable height: {type(height)}")
+  return f"<h1>Hello, World! user: {user.upper()} </h1>"
 
 
+@app.route("/welcome")
+def welcome():
+  return "<h1>Welcome ! </h1>"
+
+
+@app.route("/projects/")
+def projects():
+  return "The project page"
+
+
+@app.route("/about", methods=["GET", "POST"])
+def about():
+  if request.method == "GET":
+    return "This is a GET"
+  else:
+    return "This is a POST"
+
+
+with app.test_request_context():
+  url = "/about"
+  print(url_for('welcome'))
+  print(url_for('projects'))
+  print(url_for('about', next='/'))
+  print(url_for('hello_world', user="John Doe", age=32, height=1.75))
