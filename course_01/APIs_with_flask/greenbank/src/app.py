f154c7dfb221f6eac58a334df545dd7b4c71a82f
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-14 20:49:55 -0300

    edited file structure

--- course_01/APIs_with_flask/greenbank/src/app.py
+++ course_01/APIs_with_flask/greenbank/src/app.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'greenbank.sqlite'),
+        DATABASE="greenbank.sqlite",
     )
 
     if test_config is None:
@@ -16,13 +16,10 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+
 
     from . import db
-    
+
     db.init_app(app)
 
     return app
