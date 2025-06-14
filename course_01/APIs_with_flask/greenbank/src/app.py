44a8b79d03f56c4e26ff0c0d9a12d72306fb6d68
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-06-14 20:34:34 -0300

    updated classes for User and Post

--- course_01/APIs_with_flask/greenbank/src/app.py
+++ course_01/APIs_with_flask/greenbank/src/app.py
@@ -1,14 +1,58 @@
 import os
+from datetime import datetime
 
-from flask import Flask
+import click
+import sqlalchemy as sa
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+  pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r})"
+  
+
+class Post:
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    body: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey("user.id"))
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo('Initialized the database.')
+
+"""
+ sqlite3.register_converter(
+    "timestamp", lambda v: datetime.fromisoformat(v.decode())
+)
+"""
 
 
 def create_app(test_config=None):
-    # create and configure the app
+   
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'greenbank.sqlite'),
+        SQLALCHEMY_DATABASE_URI="sqlite:///green_bank.sqlite",
     )
 
     if test_config is None:
@@ -18,11 +62,16 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
+    #ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+   
+    #register cli commands
+    app.cli.add_command(init_db_command)
 
-
+    # initialize extensions
+    db.init_app(app)
+ 
     return app
--- course_01/APIs_with_flask/greenbank/src/db.py
+++ course_01/APIs_with_flask/greenbank/src/db.py
@@ -30,13 +30,7 @@ def init_db():
         db.executescript(f.read().decode('utf8'))
 
 
-@click.command('init-db')
-def init_db_command():
-    """Clear the existing data and create new tables."""
-    init_db()
-    click.echo('Initialized the database.')
 
-
-sqlite3.register_converter(
-    "timestamp", lambda v: datetime.fromisoformat(v.decode())
-)
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
